3256dd3cef32a006b78d315b256f1469bad5dc00
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-01-05 13:14:35 +0100

    chore: update build config

--- .config/rollup.config.js
+++ .config/rollup.config.js
@@ -37,7 +37,7 @@ export default [
                 format: 'iife',
                 sourcemap: true,
                 plugins: [terser()],
-                name: 'nord',
+                name: 'nordForms',
             },
         ],
     }),
