93666f6f14f0fc54e2533f226c9a8123b4a7f2ae
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-01-06 22:23:55 +0100

    docs: add documentation for control init type

--- src/types/control-init.d.ts
+++ src/types/control-init.d.ts
@@ -1,8 +1,40 @@
 /** @format */
 
+/**
+ * @type {ControlInit}
+ * @template {Type} - the Type of the Control
+ *
+ * Properties used to configure a control during creation.
+ */
+
 export type ControlInit<Type> = {
+    /**
+     * @type { Type | null }
+     *
+     * The initial value of the control. Set to null if the control should be empty. Values that are set here will be set inside the connected control.
+     */
     value: Type | null;
+    /**
+     * @optional
+     * @type { boolean }
+     *
+     * flag indicating if the control should be disabled
+     */
     disabled?: boolean;
+    /**
+     * @optional
+     * @type { (ev: FocusEvent) => void }
+     * @param { FocusEvent } ev
+     *
+     * Function to be executed when the control receives focus
+     */
     handleOnFocus?: (ev: FocusEvent) => void;
+    /**
+     * @optional
+     * @type { (ev: FocusEvent) => void }
+     * @param { FocusEvent } ev
+     *
+     * Function to be executed when the control loses focus
+     */
     handleOnBlur?: (ev: FocusEvent) => void;
 };
